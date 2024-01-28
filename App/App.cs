fa535c4058c18a4920c6f6b17bcc79954043154b
Luka-sama <luka-sama@pm.me>
2024-01-28 22:55:54 +0100

    Add auto-backups

--- App/App.cs
+++ App/App.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Godot;
@@ -8,6 +9,7 @@ public partial class App : Control {
 	public static TaskView View { get; private set; }
 	public static Rect2 Rect => _scrollContainer.GetGlobalRect();
 	private const string ServerUrl = "https://enveltia.net/lukovka.php?key=t8B_JR_c_u3y0t6HSabj";
+	private const string BackupDir = "user://backups";
 	private static Control _root;
 	private static TaskDetails _taskDetails;
 	private static ScrollContainer _scrollContainer;
@@ -84,6 +86,7 @@ public partial class App : Control {
 		if (string.IsNullOrEmpty(data)) {
 			return;
 		}
+		Backup(data, states);
 		foreach (var json in data.Split("\n")) {
 			try {
 				if (states) {
@@ -104,5 +107,19 @@ public partial class App : Control {
 			}
 			View.Render();
 		}
+	}
+
+	private static void Backup(string data, bool states) {
+		var date = DateOnly.FromDateTime(DateTime.Now).ToString();
+		var name = (states ? "states" : "tasks");
+		var path = $"{BackupDir}/{date}_{name}.txt";
+		if (IsMobile() || FileAccess.FileExists(path)) {
+			return;
+		}
+		if (!DirAccess.DirExistsAbsolute(BackupDir)) {
+			DirAccess.MakeDirAbsolute(BackupDir);
+		}
+		using var file = FileAccess.Open(path, FileAccess.ModeFlags.Write);
+		file.StoreString(data);
 	}
 }
